e9383772196c8c63d4d23904f479b7a6ce53116b
y-magavel <magavel.dev@gmail.com>
2022-08-11 18:31:05 +0900

    最新記事の取得先を変更
    個人ブログから取得するように変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Blog']} />
         <AboutSection sectionId="about" heading="About Me" />
         <InterestsSection sectionId="details" heading="Details" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
