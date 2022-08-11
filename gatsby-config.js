fa6b3dd60fb421ffc3f4b31cc7f2a2f1ef874e9f
y-magavel <magavel.dev@gmail.com>
2022-08-11 19:33:40 +0900

    gatsby-config.jsの修正
    サイトURLとサイト名を変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,11 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://magavel.dev/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          siteName: "Magavel", // Used in manifest.json
+          shortName: "Magavel", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
